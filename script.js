217a7961a26d294fbefd1464d87e3d55116e16f5
Karini M.Pontes <104914330+karini05@users.noreply.github.com>
2024-11-25 08:59:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,25 +5,46 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
+    
     {
-        enunciado: "Pergunta 1",
+        enunciado: "Com qual objetivo essa religião foi criada?",
         alternativas: [
-            "Alternativa 1",
-            "Aternativa 2"
+            {
+                texto: "Estabelecer a harmonia entre o homem e a natureza.",
+                afirmacao: "afirmação"
+            },
+            {
+                texto: "Para explicar a superioridade do homem sore a natureza",
+                afirmacao: "afirmação"
+            }
         ]
-    },
-    {
-        enunciado: "Pergunta 1",
-        alternativas: [
-            "Alternativa 1",
-            "Aternativa 2"
-        ]
-    },
-    {
-        enunciado: "Pergunta 1",
-        alternativas: [
-            "Alternativa 1",
-            "Aternativa 2"
-        ]
-    },
+    }
 ];
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+}
+
+mostraPergunta();
